acabaa46c1c07136d9f4ecaaf7eb0ca49eefb00d
Jack Hardy <jackhardy1@gmail.com>
2016-07-29 15:01:26 +0100

    connects to mongodb

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var database;
+var Message = mongoose.model("Message",{
+  msg: String
+});
 
 app.use(bodyParser.json());
 
+app.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.post('/api/message', function(req,res){
-  console.log(req.body);
+
+  var message = new Message(req.body);
+  message.save();
+
   res.status(200);
+});
+
+mongoose.connect("mongodb://localhost:27017/test", function(err,db){
+  if(!err){
+    console.log("we are connected to mongo");
+    database = db;
+  }
 });
 
 var server = app.listen(5000, function(){
