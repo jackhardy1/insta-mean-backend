3a4aa645c499eea53473bf2742a48cb5ba7d7f6a
Jack Hardy <jackhardy1@gmail.com>
2016-07-30 17:42:11 +0100

    logout works

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,11 +18,36 @@ module.exports = {
               messsage: err.message
               });
             }
-          res.status(200).send({token: createToken(result)});
+          res.status(200).send({token: createToken(result)
+          });
         });
-    });
-  }
+      });
+  },
+  login: function (req,res) {
+    User.findOne({
+      email: req.body.email
+        },function(err, user){
+
+          if(!user)
+            return res.status(401).send({
+              message: "Email or password invalid"
+            });
+
+          if(req.body.pwd == user.pwd) {
+            console.log(req.body, user.pwd);
+            res.send({
+              token: createToken(user)
+            });
+          } else {
+              return res.status(401).send({
+                message: "Invalid email and/or password"
+              });
+            }
+        });
+    }
 };
+
+
 
 function createToken(user){
   var payload = {
--- server.js
+++ server.js
@@ -7,20 +7,20 @@ var database;
 
 var auth = require('./controllers/auth');
 var message = require('./controllers/message');
+var checkAuthenticated = require('./services/checkAuthenticated');
+var cors = require('./services/cors');
 
 app.use(bodyParser.json());
 
-app.use(function(req,res,next){
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+app.use(cors);
 
 app.get('/api/message', message.get);
 
-app.post('/api/message', message.post);
+app.post('/api/message',checkAuthenticated, message.post);
 
 app.post('/auth/register', auth.register);
+
+app.post('/auth/login', auth.login);
 
 mongoose.connect("mongodb://localhost:27017/test", function(err,db){
   if(!err){
